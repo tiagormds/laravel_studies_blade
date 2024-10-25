83045ee33c0863d84dcd2779d54027bf10b71aa7
tiagorafael <tiagorafaelmelo@gmail.com>
2024-10-25 14:34:18 -0300

    first commit

---
+++ resources/views/layout/main_layout.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('page_title')</title>
+</head>
+<body>
+
+<h1>Texto top do layout</h1>
+
+
+@yield('content')
+
+
+<h1>Texto bottom do layout</h1>
+
+</body>
+</html>
