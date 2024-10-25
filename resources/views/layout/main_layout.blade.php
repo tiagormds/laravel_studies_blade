e16af3372bea788648231f17d9de75ac3a7bc8da
tiagorafael <tiagorafaelmelo@gmail.com>
2024-10-25 15:30:03 -0300

    Atualizações, relacionados ao bootstrap e navbar

--- resources/views/layout/main_layout.blade.php
+++ resources/views/layout/main_layout.blade.php
@@ -6,16 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('page_title')</title>
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css')  }}">
 </head>
 <body>
 
-<h1>Texto top do layout</h1>
-
+@include('layout.navbar')
 
 @yield('content')
 
 
-<h1>Texto bottom do layout</h1>
+{{-- bootstrap --}}
+<script src="{{ asset('assets/bootstrap/bootstrap.bundle.min.js') }}"></script>
 
 </body>
 </html>
