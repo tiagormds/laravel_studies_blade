1fb662901596baa193da78089ebc25ed04bfe25f
tiagorafael <tiagorafaelmelo@gmail.com>
2024-10-25 15:31:21 -0300

    Atualizacoes que faltaram

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main_layout')
+@section('page_title', 'Home Page')
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col card p-5 bg-secondary text-center">
+                Lorem ipsum dolor sit amet consectetur elit. Tempore, quo.
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,8 @@
+
+<div class="container-fluid bg-black">
+    <div class="row">
+        <div class="col p-3">
+            <h1>Barra de topo da aplicação</h1>
+        </div>
+    </div>
+</div>
